99b1248e13e753c80efebbd7b6807c9217c8a684
Alexey Kovalov <kovalov.alexey@gmail.com>
2023-03-19 22:04:27 +0000

    homework 6 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+let span = document.querySelector('.color');
+
+btnEl.addEventListener('click', () => {
+  const currentColor = getRandomHexColor();
+ 
+
+  body.style.backgroundColor = currentColor;
+
+  span.textContent = `${currentColor}`;
+})
+
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let divHeigth = 30;
+  let divWidth = 30;
+  let divArray = [];
+
+  for (let i = 1; i <= amount; i +=1) {
+    const divColor = getRandomHexColor();
+
+    const box = document.createElement('div');
+    box.style.cssText = `height: ${divHeigth}px; width: ${divWidth}px; background-color: ${divColor}`;
+    
+    divArray.push(box);
+
+    divHeigth += 10;
+    divWidth += 10;
+  }
+ 
+boxesEl.append(...divArray);
+
+}
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => createBoxes(inputEl.value))
+
+destroyBtn.addEventListener('click', () => boxesEl.innerHTML = '')
+
+
